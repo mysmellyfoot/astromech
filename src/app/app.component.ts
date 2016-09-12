af0f23a7f522e3e3f3628e08163e3aaa25daa045
Kevin Allsworth <kevinallsworth@Kevins-MacBook-Pro.local>
2016-09-12 19:52:24 +1000

    chapter 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 
+export class Astromech {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Astromech';
+  astromech: Astromech = {
+    id: 1,
+    name: 'R2D2'
+  };
 }
+
+
